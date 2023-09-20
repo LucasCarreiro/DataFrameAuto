4fe69b72dd5c47d83f27451fe0c8154493586b8a
Lucas Carreiro <lucassousacarreiro@gmail.com>
2023-09-20 00:03:16 -0300

    Update AutoData.py

--- AutoData.py
+++ AutoData.py
@@ -4,15 +4,25 @@ from tkinter import ttk
 from tkinter import messagebox
 import pandas as pd
 import threading
+from openpyxl import load_workbook
+from tkinter import Listbox
 
 # DataFrame que irá armazenar todas as colunas e linhas
 dataframe = None
 root = None
+arquivo_excel = None
+sheets = None
 
 def escolher_arquivo():
     global dataframe
     arquivo = filedialog.askopenfilename(filetypes=[("Arquivos Excel", "*.xlsx")])
     if arquivo:
+        workbook = load_workbook(arquivo_excel, read_only=True)
+        sheets = workbook.sheetnames
+        if len(sheets) == 1:
+            selecionar_colunas_para_substituir()
+        else:
+            mostrar_lista_sheets()
         # Carregar o arquivo Excel selecionado em um DataFrame
         dataframe = pd.read_excel(arquivo)
         mostrar_botao_segundo_arquivo()
@@ -22,45 +32,80 @@ def mostrar_botao_segundo_arquivo():
     selecionar_button.pack()  # Mostrar o botão para selecionar o segundo arquivo
 
 def selecionar_segundo_arquivo():
+    global arquivo_excel, sheets
     arquivo_excel = filedialog.askopenfilename(filetypes=[("Arquivos Excel", "*.xlsx")])
     if arquivo_excel:
-        # Ler o arquivo Excel selecionado em um DataFrame
-        dataframe_excel = pd.read_excel(arquivo_excel)
+        # Carregar as planilhas (sheets) do arquivo Excel selecionado
+        workbook = load_workbook(arquivo_excel, read_only=True)
+        sheets = workbook.sheetnames
+        if len(sheets) == 1:
+            selecionar_colunas_para_substituir()
+        else:
+            mostrar_lista_sheets()
 
-        # Substituir colunas correspondentes no DataFrame Excel
-        for coluna in dataframe.columns:
-            if coluna in dataframe_excel.columns:
-                dataframe_excel[coluna] = dataframe[coluna]
+def mostrar_lista_sheets():
+    global root
+    root.withdraw()  # Esconder a janela principal
+    sheet_selection_window = tk.Tk()
+    sheet_selection_window.title("Selecione as Sheets")
+    sheet_selection_window.geometry("300x200")
 
-        # Criar uma barra de progresso
-        progress_bar = ttk.Progressbar(root, length=300, mode='indeterminate')
-        progress_bar.pack()
-        progress_bar.start()
+    sheet_listbox = Listbox(sheet_selection_window, selectmode=tk.MULTIPLE)
+    for sheet in sheets:
+        sheet_listbox.insert(tk.END, sheet)
+    sheet_listbox.pack()
 
-        # Função para realizar as operações em segundo plano
-        def processamento_em_segundo_plano():
-            # Simule algum processamento demorado (você pode remover isso)
-            import time
-            time.sleep(5)
+    def confirmar_selecao():
+        selected_sheets = [sheets[i] for i in sheet_listbox.curselection()]
+        sheet_selection_window.destroy()
+        selecionar_colunas_para_substituir(selected_sheets)
 
-            progress_bar.stop()  # Parar a barra de progresso
-            progress_bar.pack_forget()  # Remover a barra de progresso
-            messagebox.showinfo("Concluído", "Análise e Modificações concluídas com sucesso!")
+    confirm_button = tk.Button(sheet_selection_window, text="Confirmar", command=confirmar_selecao)
+    confirm_button.pack()
 
-            # Pedir ao usuário para escolher onde salvar o arquivo resultante
+def selecionar_colunas_para_substituir(selected_sheets=None):
+    if selected_sheets is None:
+        selected_sheets = sheets
+
+    global root, dataframe, arquivo_excel
+
+    root.withdraw()  # Esconder a janela principal
+    coluna_selection_window = tk.Tk()
+    coluna_selection_window.title("Selecione as Colunas para Substituir")
+    coluna_selection_window.geometry("400x400")
+
+    coluna_listbox = Listbox(coluna_selection_window, selectmode=tk.MULTIPLE)
+    for coluna in dataframe.columns:
+        coluna_listbox.insert(tk.END, coluna)
+    coluna_listbox.pack()
+
+    def confirmar_substituicao():
+        selected_columns = [coluna_listbox.get(i) for i in coluna_listbox.curselection()]
+
+        # Processar cada sheet selecionada
+        for sheet_name in selected_sheets:
+            # Ler o arquivo Excel selecionado em um DataFrame
+            dataframe_excel = pd.read_excel(arquivo_excel, sheet_name=sheet_name)
+
+            # Substituir colunas correspondentes no DataFrame Excel
+            for coluna in selected_columns:
+                if coluna in dataframe_excel.columns:
+                    dataframe_excel[coluna] = dataframe[coluna]
+
+            # Salvar o DataFrame Excel resultante em um novo arquivo
             arquivo_salvar = filedialog.asksaveasfilename(defaultextension=".xlsx", filetypes=[("Arquivos Excel", "*.xlsx")])
             if arquivo_salvar:
-                dataframe_excel.to_excel(arquivo_salvar, index=False)
+                with pd.ExcelWriter(arquivo_salvar, engine='openpyxl') as writer:
+                    writer.book = load_workbook(arquivo_excel)
+                    writer.sheets = {ws.title: ws for ws in writer.book.worksheets}
+                    dataframe_excel.to_excel(writer, index=False, sheet_name=sheet_name)
 
-            voltar_ao_inicio()
+        coluna_selection_window.destroy()
+        root.deiconify()  # Mostrar a janela principal
+        messagebox.showinfo("Concluído", "Análise e Modificações concluídas com sucesso!👍")
 
-        # Iniciar uma thread para o processamento em segundo plano
-        processing_thread = threading.Thread(target=processamento_em_segundo_plano)
-        processing_thread.start()
-
-def voltar_ao_inicio():
-    selecionar_button.pack_forget()  # Remover o botão de seleção do segundo arquivo
-    escolher_button.pack()  # Mostrar o botão de escolher a base primária novamente
+    confirm_button = tk.Button(coluna_selection_window, text="Confirmar Substituição", command=confirmar_substituicao)
+    confirm_button.pack()
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -69,6 +114,6 @@ if __name__ == "__main__":
     escolher_button = tk.Button(root, text="Escolha a Base Primaria", command=escolher_arquivo)
     escolher_button.pack()
 
-    selecionar_button = tk.Button(root, text="Escolha a Base Secundaria", command=selecionar_segundo_arquivo)
-    
+    selecionar_button = tk.Button(root, text="Selecionar Segundo Arquivo", command=selecionar_segundo_arquivo)
+
     root.mainloop()
